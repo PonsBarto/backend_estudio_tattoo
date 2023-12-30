86547a941d01690afeea258e82eafdb71bd9d2fd
Barto <ponsbarto@gmail.com>
2023-12-30 11:25:08 +0100

    Created the structure of migrations and models

---
+++ src/migrations/1703852835645-CreateUser.ts
@@ -0,0 +1,12 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateUser1703852835645 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
